be58c5ce5b7b5c5568b66c881fe5f1ad51e28ecf
Stefanie Soares da Silva <s.soares.da.silva@avanade.com>
2024-01-15 18:18:10 -0300

    Commit Inicial

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,13 +1,10 @@
 ﻿
 using SOLID._02___O.Boa_prática;
 
-
-#region Open/Closed
 var taxaEUR = new Taxa(new TaxaEUR());
 taxaEUR.moeda = "EUR";
 taxaEUR.valor = 50;
 taxaEUR.CalcularTarifa();
-#endregion
 
 
 
